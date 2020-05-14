e3761d1ec9fb29d2577e47d57add7a79e7361187
nely de faria <nelly.nf54@gmail.com>
2020-05-14 20:21:10 -0300

    03 - Add folder dados and file cursos.php

---
+++ Project_PHP/dados/cursos.php
@@ -0,0 +1,30 @@
+<?php
+
+function cursos(){
+  return[
+    "php" => [
+      "nome_curso" => "Curso de PHP Fundamentos",
+      "versao_ferramenta" => 7.4,
+      "carga_horaria" => 40,
+      "status" => true
+      ],
+      "java" => [
+      "nome_curso" => "Curso de Java",
+      "versao_ferramenta" => 11.4,
+      "carga_horaria" => 40,
+      "status" => true
+      ],
+      "C#" => [
+        "nome_curso" => "Curso de C#",
+        "versao_ferramenta" => 8,
+        "carga_horaria" => 50,
+        "status" => true
+      ],
+      "javascript" => [
+        "nome_curso" => "Curso de JavaScript",
+        "versao_ferramenta" => 5,
+        "carga_horaria" => 50,
+        "status" => false
+        ]
+  ];
+}
